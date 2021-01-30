eb8bfa10f861e736b79d9beed7cdbead2cb1ac33
Shovra Das <shovradas@gmail.com>
2021-01-30 13:10:26 +0100

    #2: Add new Student added

---
+++ AddStudent.java
@@ -0,0 +1,14 @@
+class Student{
+    String id;
+    String name;
+}
+
+class AddStudent{
+    public static void main(String[] args){
+        Student student = new Student();
+        student.id = "215s45";
+        student.name = "Bob";
+
+        // Code Save
+    }
+}
